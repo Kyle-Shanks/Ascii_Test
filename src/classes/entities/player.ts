08ab5dac3e797d51da7ddb5d19060e9407d05049
KJ Shanks <kyle.j.shanks@gmail.com>
2021-07-26 18:29:13 -0400

    Refactor Player class a bit

--- src/classes/entities/player.ts
+++ src/classes/entities/player.ts
@@ -1,5 +1,5 @@
 import Actor, { ActorProps } from 'src/classes/entities/actor'
-import { InputEvent } from 'src/classes/input'
+import { InputEvent, InputKey } from 'src/classes/input'
 import Map from 'src/classes/map'
 import { ENTITY_TYPES } from 'src/core/constants'
 import { Vector2 } from 'src/core/types'
@@ -8,43 +8,48 @@ interface PlayerProps extends ActorProps {
     // Any player specific props go here
 }
 
+type ActionMap = Record<InputKey, (map: Map) => void>
+
 class Player extends Actor {
+    _pressActionMap: ActionMap
+    _releaseActionMap: ActionMap
+
     constructor(props: PlayerProps) {
         super(props)
         this.type = ENTITY_TYPES.PLAYER
+
+        this._pressActionMap = {
+            'W': (map: Map) => this._updatePosition(Vector2.UP, map),
+            'A': (map: Map) => this._updatePosition(Vector2.LEFT, map),
+            'S': (map: Map) => this._updatePosition(Vector2.DOWN, map),
+            'D': (map: Map) => this._updatePosition(Vector2.RIGHT, map),
+            'Shift': (map: Map) => {},
+            'Space': (map: Map) => {},
+            'J': (map: Map) => {},
+            'K': (map: Map) => {},
+        }
+        this._releaseActionMap = {
+            'W': (map: Map) => {},
+            'A': (map: Map) => {},
+            'S': (map: Map) => {},
+            'D': (map: Map) => {},
+            'Shift': (map: Map) => {},
+            'Space': (map: Map) => {},
+            'J': (map: Map) => {},
+            'K': (map: Map) => {},
+        }
     }
 
     update = (event: InputEvent, map: Map) => {
-        if (event.type === 'press') {
-            let dir: Vector2 | null = null
-            switch (event.key) {
-                case 'W':
-                    dir = Vector2.UP
-                    break
-                case 'A':
-                    dir = Vector2.LEFT
-                    break
-                case 'S':
-                    dir = Vector2.DOWN
-                    break
-                case 'D':
-                    dir = Vector2.RIGHT
-                    break
-            }
-
-            if (dir) this._updatePosition(dir, map)
-        } else if (event.type === 'release') {
-            switch (event.key) {}
-        }
+        if (event.type === 'press') this._pressActionMap[event.key](map)
+        else if (event.type === 'release') this._releaseActionMap[event.key](map)
     }
 
     private _updatePosition = (dir: Vector2, map: Map) => {
         const newPosition = this.position.add(dir)
         const entityAtPosition = map.getAtPosition(newPosition)
 
-        if (entityAtPosition !== null && entityAtPosition.isSolid()) return
-
-        this.position = newPosition
+        if (entityAtPosition === null || !entityAtPosition.isSolid()) this.position = newPosition
     }
 }
 
